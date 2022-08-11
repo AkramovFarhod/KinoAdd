f92b0bb94f21e98c29b07d96bc138fb49950ddb7
Farhod <fara888055559@gmail.com>
2022-08-11 16:05:26 +0500

    lesson-1

--- app/src/main/java/com/example/kinoadd/MainActivity.kt
+++ app/src/main/java/com/example/kinoadd/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.kinoadd.navigation.SetupNavHost
 import com.example.kinoadd.ui.theme.KinoAddTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,25 +19,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KinoAddTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                val navController = rememberNavController()
+                SetupNavHost(navController = navController)
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    KinoAddTheme {
-        Greeting("Android")
-    }
-}
